edf47e4ebf0b4c6a1cb1fd5ff71e960748f8292e
andresort18 <andresortguerra15@gmail.com>
2025-04-10 10:05:45 -0600

    Pagina Beta terminada

--- Js/contacto.js
+++ Js/contacto.js
@@ -1,2 +1,8 @@
-
-
+//animaciones header
+document.addEventListener("DOMContentLoaded", function () {
+    // Agregar clase 'visible' para animar la entrada del header
+    const header = document.querySelector("header");
+    setTimeout(() => {
+        header.classList.add("visible");
+    }, 300); // Retraso para que se vea más fluido
+});
